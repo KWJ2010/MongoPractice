03fb5a409f2c745120fce7840fa2f1a36c2b2f71
Kyle J <kjohnson@epcon.com>
2017-04-10 21:00:41 -0500

    working on hw3

--- MongoPractice/Program.cs
+++ MongoPractice/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,67 @@ namespace MongoPractice
     {
         static void Main(string[] args)
         {
+            MainAsync(args).Wait();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
         }
+
+        static async Task MainAsync(string[] args)
+        {
+            //ConnectionString
+            var connectionString = "mongodb://localhost:27017";
+
+            //First createt the client. singleton in IoC container or 
+            //static instance like this are good approaches
+            var client = new MongoClient(connectionString);
+
+            //Both the db and col are thread safe so they, like the client, can be stored globaly
+            var db = client.GetDatabase("school");
+            var col = db.GetCollection<BsonDocument>("students");
+
+            //Method 1 
+            #region one way to build a collection
+            //using (var cursor = await col.Find(new BsonDocument()).ToCursorAsync())
+            //{
+            //    while (await cursor.MoveNextAsync())
+            //    {
+            //        foreach (var student in cursor.Current)
+            //        {
+            //            if (true)
+            //            {
+
+            //            }
+
+            //            Console.WriteLine(student);
+            //        }
+            //    }
+            //}
+            #endregion
+
+            //Method 2, store in memory as list
+            #region another way to build a collection
+            //var list = await col.Find(new BsonDocument()).ToListAsync();
+            //foreach (var doc in list)
+            //{
+            //    Console.WriteLine(doc);
+            //}
+            #endregion
+
+            //Method 3
+            #region Final way to build a collection
+            //Foreach document, run the callback method passed into th ForEachAsync
+            //await col.Find(new BsonDocument()).ForEachAsync(doc => Console.WriteLine(doc));
+            #endregion
+
+            //student.scores["type": "homework" ]
+            //foreach student, sort homework lowest to highest, drop first, then search db for highest hw grade
+
+
+            var list = await col.Find(new BsonDocument())
+                .Sort("{scores.type: 1}")
+                .ToListAsync();
+
+
+    }
     }
 }
